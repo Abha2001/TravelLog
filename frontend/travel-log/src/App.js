75569a9ccf5ec7fcb81a6b5f5a3ad22fae5fb8fe
Abha Jha <jhaabha08@gmail.com>
2020-06-04 01:37:55 +0530

    added the map

--- frontend/travel-log/src/App.js
+++ frontend/travel-log/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
+import ReactMapGL from 'react-map-gl';
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 24.1129,
+    longitude: 77.2345,
+    zoom: 4,
+  });
+
   return (
-    <div className="App">
-      
-    </div>
+    <ReactMapGL
+      mapStyle={'mapbox://styles/abhajha/ckazrvvd202js1io946sfgcjl'}
+      // enter your mapboxAPIAccessToken
+      mapboxApiAccessToken={}
+      {...viewport}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+    />
   );
 }
 
