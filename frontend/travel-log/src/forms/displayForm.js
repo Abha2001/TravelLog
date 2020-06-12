902b9b9ba7a5bdf448e49edbe5cf7a97d30db0d0
Abha Jha <jhaabha08@gmail.com>
2020-06-13 00:47:04 +0530

    form for adding entries

--- frontend/travel-log/src/forms/displayForm.js
+++ frontend/travel-log/src/forms/displayForm.js
@@ -10,6 +10,7 @@ function DisplayForm({place}){
             </h3>
             <img className="place-image" src={place.image}></img>
             <div>
+            {place.comment?<h4 className="comment">{place.comment}</h4>:null}
                 <small className="rating">Rating:{place.rating}</small>
                 <img className="star" src="https://cdn2.iconfinder.com/data/icons/modifiers-add-on-1-flat/48/Mod_Add-On_1-35-512.png"></img>
                 <div className="details">
@@ -17,7 +18,13 @@ function DisplayForm({place}){
                         Visited On: {moment(place.dateOfVisit).format("MMM Do YY")}
                     </h4>
                     <h5><small>Posted:</small>{moment(place.createdAt).fromNow()}</h5>
-                    <h5><small>Edited:</small>{moment(place.updatedAt).fromNow()}</h5>
+                    {
+                    moment(place.createdAt).fromNow()!==moment(place.updatedAt).fromNow()?
+                     <h5>
+                        <small>Edited:</small>
+                        {moment(place.updatedAt).fromNow()}
+                    </h5>:null
+                    }
                 </div>
             </div>
         </div>
